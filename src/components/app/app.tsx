8569f733f384d2e9d3efd318fb8ecb76a66cd8c9
Alexandra Glen <varyandis@yandex.ru>
2024-12-17 16:13:20 +0300

    Заменит Route на PrivateRoute

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,11 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
 import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 import OfferScreen from '../../pages/offer-screen/offer-screen';
+import PrivateRoute from '../private-route/private-route';
 
 
 type AppScreenProps = {
@@ -25,7 +26,11 @@ function App(props : AppScreenProps) {
         />
         <Route
           path={AppRoute.Favorites}
-          element={<FavoritesScreen/>}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <FavoritesScreen/>
+            </PrivateRoute>
+          }
         />
         <Route
           path={AppRoute.Offer}
