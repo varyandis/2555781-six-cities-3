974521f59a83a7955a2b7acb81013c6c3a468346
александра <varyandis@yandex.ru>
2024-12-26 15:59:50 +0300

    Подключит моки в файл `index.tsx`

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { Settings } from './const';
+import { offers } from './mocks/offers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <App
       offersCount = {Settings.offersCount}
+      offers = {offers}
     />
   </React.StrictMode>
 );
