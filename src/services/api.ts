b26a114a18a47848d26c1dd9b0ac6af22f1d1d8b
Alexandra Glen <varyandis@yandex.ru>
2025-03-03 13:58:08 +0300

    Добавим поддержку токенов

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,24 @@
 import axios from 'axios';
 import { AxiosInstance } from 'axios';
+import { getToken } from './token';
 
 const BACKEND_URL = 'https://15.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = (): AxiosInstance => {
-  const api = axios.create(
-    {
-      baseURL: BACKEND_URL,
-      timeout: REQUEST_TIMEOUT,
+  const api = axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT,
+  });
+
+  api.interceptors.request.use((config) => {
+    const token = getToken();
+
+    if (token && config.headers) {
+      config.headers['x-token'] = token;
     }
-  );
+
+    return config;
+  });
   return api;
 };
