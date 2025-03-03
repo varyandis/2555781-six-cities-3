4c12a5b41388913090dc31c1847574b0e91a10a5
Alexandra Glen <varyandis@yandex.ru>
2025-03-03 13:35:02 +0300

    Сконфигурирует пакет axios

---
+++ src/services/api.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { AxiosInstance } from 'axios';
+
+const BACKEND_URL = 'https://15.design.htmlacademy.pro/six-cities';
+const REQUEST_TIMEOUT = 5000;
+
+export const createApi = (): AxiosInstance => {
+  const api = axios.create(
+    {
+      baseURL: BACKEND_URL,
+      timeout: REQUEST_TIMEOUT,
+    }
+  );
+  return api;
+};
