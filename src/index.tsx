521954147b20bac395342151c58508dea1aaf8b6
Alexandra Glen <varyandis@yandex.ru>
2025-03-05 18:33:31 +0300

    Создаст компонент для отображения ошибки и подключит его

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { offers } from './mocks/offers';
 import { offersNearby } from './mocks/offers-nearby';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import ErrorMessage from './components/error-message/error-message';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,6 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ErrorMessage />
       <App
         offers={offers}
         offersNearby={offersNearby}
